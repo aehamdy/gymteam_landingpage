2f948714abfe2d38d10b08f2ca204854bb4d59f3
aehamdy <ae.hamdy91@gmail.com>
2024-10-07 21:21:59 +0300

    Add custom color palette to tailwind config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-500">Hello there!</h1>
-      <h1 className="text-blue-500">Hello there!</h1>
+      <h1 className="text-accent">Hello there!</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        'accent': 'rgb(214, 253, 82)',
+        'primary-bg': 'rgb(0, 0, 0)',
+        'secondary-bg': 'rgb(36, 36, 36)',
+        'heading': 'rgb(245, 245, 245)',
+        'highlight': 'rgb(221, 140, 121)',
+        'primary-paragraph': '#242424',
+        'secondary-paragraph': 'rgb(57, 57, 57)',
+        'toggle-bg': 'rgb(242, 201, 77)',
+      },
+    },
   },
   plugins: [],
 }

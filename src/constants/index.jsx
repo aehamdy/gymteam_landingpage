a276f0ed7a0671c36bd937a6f1638dbad136ab32
aehamdy <ae.hamdy91@gmail.com>
2024-10-09 01:15:08 +0300

    feat: Create Coaches component

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -5,6 +5,10 @@ import feature2 from "../assets/features-2.png";
 import feature3 from "../assets/features-3.png";
 import about1 from "../assets/about-1.jpg";
 import about2 from "../assets/about-2.jpg";
+import coach1 from "../assets/coach-1.jpg";
+import coach2 from "../assets/coach-2.jpg";
+import coach3 from "../assets/coach-3.jpg";
+import coach4 from "../assets/coach-4.jpg";
 
 export const navItems = [
   { label: "Home", href: "#" },
@@ -35,3 +39,26 @@ export const features = [
 ];
 
 export const aboutImages = [{ img: about1 }, { img: about2 }];
+
+export const coaches = [
+  {
+    img: coach1,
+    name: "Jane Cooper",
+    type: "Corssfit Coach",
+  },
+  {
+    img: coach2,
+    name: "Brooklyn Simmons",
+    type: "Corssfit Coach",
+  },
+  {
+    img: coach3,
+    name: "Savannah Nguyen",
+    type: "Corssfit Coach",
+  },
+  {
+    img: coach4,
+    name: "Kathryn Murphy",
+    type: "Corssfit Coach",
+  },
+];

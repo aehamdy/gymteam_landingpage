fd331b7a8890300a85fe56c33db650dfd5ef8497
aehamdy <ae.hamdy91@gmail.com>
2024-10-08 17:02:51 +0300

    feat: Add features elements array in index.jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,6 +1,27 @@
+import feature1 from "../assets/features-1.png";
+import feature2 from "../assets/features-2.png";
+import feature3 from "../assets/features-3.png";
 export const navItems = [
   { label: "Home", href: "#" },
   { label: "Shop", href: "#" },
   { label: "Blog", href: "#" },
   { label: "Contact", href: "#" },
+];
+
+export const features = [
+  {
+    img: feature1,
+    heading: "muscles building",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum asperiores molestiae at ut, ad itaque, soluta aut possimus veritatis impedit quod nesciunt ullam obcaecati, dolores sapiente provident nostrum numquam deserunt.",
+  },
+  {
+    img: feature2,
+    heading: "muscles building",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum asperiores molestiae at ut, ad itaque, soluta aut possimus veritatis impedit quod nesciunt ullam obcaecati, dolores sapiente provident nostrum numquam deserunt.",
+  },
+  {
+    img: feature3,
+    heading: "muscles building",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum asperiores molestiae at ut, ad itaque, soluta aut possimus veritatis impedit quod nesciunt ullam obcaecati, dolores sapiente provident nostrum numquam deserunt.",
+  },
 ];

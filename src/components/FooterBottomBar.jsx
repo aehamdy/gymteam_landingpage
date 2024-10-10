fa953ee57cee82fa7ed6130a0928236a74524670
aehamdy <ae.hamdy91@gmail.com>
2024-10-10 18:29:30 +0300

    style: Adjust font size on different screens

--- src/components/FooterBottomBar.jsx
+++ src/components/FooterBottomBar.jsx
@@ -1,9 +1,13 @@
 function FooterBottomBar() {
   return (
     <div className="py-5 px-section-horizontal-padding-sm md:px-section-horizontal-padding-md lg:px-section-horizontal-padding-lg text-secondary-paragraph border-t-2 border-t-[#252525]">
-      <div className="flex flex-col md:flex-row items-center justify-between gap-2 px-section-horizontal-padding">
-        <p>Copyright 2024 All Right Reserved</p>
-        <p className="hover:text-white duration-short">Made with ❤️ by AE</p>
+      <div className="flex flex-row items-center justify-between gap-2 px-section-horizontal-padding">
+        <p className="text-xs md:text-base hover:text-white duration-short">
+          Copyright 2024 All Right Reserved
+        </p>
+        <p className="text-xs md:text-base hover:text-white duration-short">
+          Made with ❤️ by AE
+        </p>
       </div>
     </div>
   );

4647dfe418a688ea99e4ec34a79b24cb121c9a37
aehamdy <ae.hamdy91@gmail.com>
2024-10-10 18:41:40 +0300

    style: Change font size on mobile screen

--- src/components/FooterBottomBar.jsx
+++ src/components/FooterBottomBar.jsx
@@ -1,13 +1,11 @@
 function FooterBottomBar() {
   return (
     <div className="py-5 px-section-horizontal-padding-sm md:px-section-horizontal-padding-md lg:px-section-horizontal-padding-lg text-secondary-paragraph border-t-2 border-t-[#252525]">
-      <div className="flex flex-row items-center justify-between gap-2 px-section-horizontal-padding">
-        <p className="text-xs md:text-base hover:text-white duration-short">
+      <div className="flex flex-row items-center justify-between gap-2 px-section-horizontal-padding text-[0.6rem] md:text-base">
+        <p className="hover:text-white duration-short">
           Copyright 2024 All Right Reserved
         </p>
-        <p className="text-xs md:text-base hover:text-white duration-short">
-          Made with ❤️ by AE
-        </p>
+        <p className="hover:text-white duration-short">Made with ❤️ by AE</p>
       </div>
     </div>
   );

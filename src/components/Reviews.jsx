af8c130dd72fb9a3073b92d471c13ca00c158502
aehamdy <ae.hamdy91@gmail.com>
2024-10-10 16:33:29 +0300

    style: Adjust styles for Reviews section

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,27 +1,31 @@
 import { Quote } from "lucide-react";
 import { reviews } from "../constants";
+import SectionHeading from "./SectionHeading";
 
 function Reviews() {
   return (
-    <section className="flex justify-end md:justify-start items-end w-full h-[450px] my-section-spacing py-4 px-5 bg-[url('./assets/reviews.jpg')] bg-cover bg-bottom bg-no-repeat">
-      <div className="w-full md:w-2/3 py-4 px-2 rounded-xl bg-accent">
-        {reviews.map((review, i) => (
-          <article
-            key={i}
-            className="flex flex-col gap-3 py-3 px-3 font-orbitron uppercase font-extrabold text-black"
-          >
-            <Quote />
-            <p>{review.text}</p>
-            <div>
+    <section className="my-section-spacing px-section-horizontal-padding overflow-hidden">
+      <SectionHeading text="review from clients" />
+      <div className="w-full flex justify-end items-end h-[350px] py-4 px-5 bg-[url('./assets/reviews.jpg')] bg-cover bg-center bg-no-repeat rounded-3xl">
+        <div className="w-full md:w-1/2 py-4 px-2 rounded-xl bg-accent">
+          {reviews.map((review, i) => (
+            <article
+              key={i}
+              className="flex flex-col gap-3 py-3 px-3 font-orbitron uppercase font-extrabold text-black"
+            >
+              <Quote />
+              <p>{review.text}</p>
               <div>
-                <h3>{review.name}</h3>
-                <span className="font-sans font-normal">
-                  {review.profession}
-                </span>
+                <div>
+                  <h3 className="text-sm">{review.name}</h3>
+                  <span className="font-sans font-normal">
+                    {review.profession}
+                  </span>
+                </div>
               </div>
-            </div>
-          </article>
-        ))}
+            </article>
+          ))}
+        </div>
       </div>
     </section>
   );

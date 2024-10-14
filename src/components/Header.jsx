c6541f71d185de96ad3fc66c715bba694a7fdeff
aehamdy <ae.hamdy91@gmail.com>
2024-10-15 01:32:45 +0300

    fix: Resolve the issue of stacking context o Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Nav from "./Nav";
 function Header() {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <header className="absolute w-full top-0 z-50 py-3 px-4 md:px-6 lg:px-12 backdrop-blur-lg flex justify-between items-center">
+    <header className="sticky w-full top-0 z-50 py-3 px-4 md:px-6 lg:px-12 backdrop-blur-lg flex justify-between items-center">
       <Logo />
       <Nav isExpanded={isExpanded} />
       <HeaderIcons setIsExpanded={setIsExpanded} isExpanded={isExpanded} />
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import SecondaryButton from "./SecondaryButton";
 
 function HeroSection() {
   return (
-    <section className="relative grid place-items-center before:absolute before:content[''] before:top-0 before:start-0 before:w-full before:h-full before:bg-black/[0.8] h-dvh bg-[url('/src/assets/hero-section.jpg')] bg-cover bg-no-repeat bg-center">
+    <section className="relative grid place-items-center z-40 mt-[-3.5rem] lg:mt-[-4.6rem] before:absolute before:content[''] before:top-0 before:start-0 before:w-full before:h-full before:bg-black/[0.8] h-dvh bg-[url('/src/assets/hero-section.jpg')] bg-cover bg-no-repeat bg-center">
       <div className="absolute flex flex-col gap-7 md:gap-5 w-3/4 md:1/2 text-center">
         <h2 className="w-fit mx-auto font-orbitron font-extrabold text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white uppercase text-center tracking-wider">
           work with professionals

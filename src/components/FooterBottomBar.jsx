5285e0ee72f11c808a26da8bf442e9ab72c248de
aehamdy <ae.hamdy91@gmail.com>
2024-11-02 18:59:03 +0200

    feat: Add my portfolio's link to FooterBottom

--- src/components/FooterBottomBar.jsx
+++ src/components/FooterBottomBar.jsx
@@ -1,11 +1,11 @@
 function FooterBottomBar() {
   return (
     <div className="py-5 px-section-horizontal-padding-sm md:px-section-horizontal-padding-md lg:px-section-horizontal-padding-lg text-secondary-paragraph border-t-2 border-t-[#252525]">
-      <div className="flex flex-row items-center justify-between gap-2 px-section-horizontal-padding text-[0.6rem] md:text-base">
+      <div className="flex flex-row items-center justify-between gap-2 px-section-horizontal-padding text-[0.6rem] md:text-base select-none">
         <p className="hover:text-white duration-short">
           Copyright 2024 All Right Reserved
         </p>
-        <p className="hover:text-white duration-short">Made with ❤️ by AE</p>
+        <p className="group hover:text-white duration-short">Made with ❤️ by <a href="https://ahmedessam-portfolio.vercel.app/" target="_blank" className="text-accent group-hover:underline">AE</a></p>
       </div>
     </div>
   );

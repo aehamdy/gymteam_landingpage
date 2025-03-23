a669eb60763b489eb60c9be2fa056159bba4e44e
aehamdy <ae.hamdy91@gmail.com>
2025-03-24 01:53:53 +0200

    Refactor: Display whether a element or button element depending on props

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,9 +1,22 @@
 /* eslint-disable react/prop-types */
-function PrimaryButton({ text }) {
-  return (
-    <button className="w-fit h-fit font-orbitron font-extrabold uppercase tracking-wide py-1.5 px-5 rounded-3xl text-black bg-accent hover:shadow-button-shadow duration-short cursor-pointer">
+function PrimaryButton({ text, href }) {
+  const styles =
+    "w-fit h-fit font-orbitron font-extrabold uppercase tracking-wide py-1.5 px-5 rounded-3xl text-black bg-accent hover:shadow-button-shadow duration-short cursor-pointer";
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const section = document.getElementById(href);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return href ? (
+    <a href={href} onClick={handleClick} className={styles}>
       {text}
-    </button>
+    </a>
+  ) : (
+    <button className={styles}>{text}</button>
   );
 }
 

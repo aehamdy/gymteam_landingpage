b4d74a53a460d15ee0bb493c490a8fc9a9ddb850
aehamdy <ae.hamdy91@gmail.com>
2025-03-24 03:03:16 +0200

    Refactor: Adjust coaches container columns

--- src/components/Coaches.jsx
+++ src/components/Coaches.jsx
@@ -10,9 +10,14 @@ function Coaches() {
         <br />
         Pharetra enim sed neque dolor viverra.
       </p>
-      <div className="grid grid-col-1 place-items-center md:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-10 lg:gap-16">
+
+      <div className="flex items-center gap-4 snap-x lg:snap-none snap-mandatory lg:snap-mandatory overflow-x-scroll overflow-y-hidden lg:overflow-x-visible scrollbar-hide">
+        {/* <div className="grid grid-col-1 place-items-center md:grid-cols-2 lg:grid-cols-4 gap-5 md:gap-10 lg:gap-4"> */}
         {coaches.map((coach, i) => (
-          <article key={i} className="flex flex-col gap-2 w-[200px]">
+          <article
+            key={i}
+            className="flex flex-col gap-2 snap-start shrink-0 w-[calc(100%/2)] md:w-[calc(100%/3)] lg:w-[calc(100%/4)]"
+          >
             <div className="rounded-xl overflow-hidden">
               <img
                 src={coach.img}
@@ -21,7 +26,7 @@ function Coaches() {
               />
             </div>
             <div className="">
-              <h3 className="font-bold tracking-wide">{coach.name}</h3>
+              <h3 className="font-bold text-sm tracking-wide">{coach.name}</h3>
               <p className="text-secondary-paragraph">{coach.type}</p>
             </div>
           </article>
